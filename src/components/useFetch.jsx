92da71b6922c6dce8cf09cb1386d6cec1ebe046c
Mohammed Oladimeji Abdulrazaq <abdulrazaq@gmail.com>
2022-09-29 13:37:11 +0100

    Endpoint added

--- src/components/useFetch.jsx
+++ src/components/useFetch.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
