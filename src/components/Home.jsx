d84915ac59f26e993b8bda892d43d0063d5a4b5b
Mohammed Oladimeji Abdulrazaq <abdulrazaq@gmail.com>
2022-09-29 13:38:43 +0100

    Endpoint added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import Question from "./Question";
 import useFetch from "./useFetch";
 
 const Home = async () =>{
-   const { data: questions, isLoading, error } = await useFetch('https://paper-grizzly-millennium.glitch.me/db.json');
-
+   const { data: questions, isLoading, error } = useFetch('https://paper-grizzly-millennium.glitch.me/db.json');
+    console.log(data);
     return(
         <div className="home">
             { error && <div> { error } </div> }
